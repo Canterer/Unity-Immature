8d3eea84032e17422321508d6bb4bafb323a26b4
Canterer <1170836331@qq.com>
2018-03-21 18:16:27 +0800

    Canterer 自定义Log

--- Assets/Scripts/Lua/Base/ZSLog.lua
+++ Assets/Scripts/Lua/Base/ZSLog.lua
@@ -22,8 +22,7 @@
 --         -- 打印父节点
 --         local track_info = debug.getinfo(2, "Sln")
 --         local str = string.format("From %s:%d in function `%s`", track_info.short_src,track_info.currentline,track_info.name or "")
-
---         Log.Debug(string.format("%s\n%s", table.concat(new_args, " "), str))
+--         Debug.Log(string.format("%s\n%s", table.concat(new_args, " "), str))
 --     end
 -- end
 
@@ -38,8 +37,12 @@ function ZSInit()
 	ZS_SendProto_Count = 0
 	ZS_RecvProto_Count = 0
 
-	ZS_ZS_Count = 1 --插针
-	ZS_Log_Color = "ff00ff"
+	ZS_ZS_Count = 0 --插针
+	ZS_Color_ZS = "#FFCC44"
+	ZS_Color_Log = "#7CFF88"
+	ZS_Color_Table = "#FF27C9"
+	ZS_Color_Proto_Send = "#FF7733"
+	ZS_Color_Proto_Recv = "#FF6677"
 
 
 	-- 协议控制
@@ -50,6 +53,7 @@ function ZSInit()
 	ZS_White_List = {
 		-- { protoHead = 98 }, -- 监听该协议头
 		-- { protoHead = 98 }, -- 监听该协议头
+		{ min = 100, max = 111 }, --测试
 		{ min = 9802, max = 9804 }, --"活"字模块
 		{ min = 14600, max = 14605 }, --监听该区域协议
 		-- { min = 9802, max = 9803 }, --监听该区域协议
@@ -60,23 +64,37 @@ function ZSInit()
 		-- { min = 2},--
 	}
 end
+
 ZSInit()
 
-function ZS()
-	ZS_ZS_Count = ZS_ZS_Count == 9 and 0 or (ZS_ZS_Count+1)
+function ZS(...)
+	local num_args = select("#", ...)	
 	local track_info = debug.getinfo(2, "Sln")
-	ZS_String = string.format("From %s:%d ## %s", track_info.short_src, track_info.currentline, ZSRepeart( ZS_ZS_Count,15))
-	Log.Debug( ZSRepeart("<color=#ffff00>")..ZS_String..ZSRepeart("</color>") )
+	ZS_String = string.format("From %s:%d ## ", track_info.short_src, track_info.currentline)
+	if num_args == 0 then
+		ZS_ZS_Count = ZS_ZS_Count == 9 and 0 or (ZS_ZS_Count+1)
+		ZS_String = ZS_String..ZSRepeart( ZS_ZS_Count,15)
+	else
+		for i = 1, num_args do
+	        local arg = select(i, ...)
+	        ZS_String = ZS_String.." "..tostring(arg)
+    	end
+	end
+
+	ZS_Debug_Log( ZS_String, ZS_Color_ZS )
+	ZS_String = ""
 end
 
+-- 一个参数时  字符串 打印变量名为该字符串的本地变量
+-- 两个参数时	Table,字符串 打印Table[字符串]
+-- 两个参数时	字符串,变量	 打印 字符串 = 变量
 function ZSLog(...)
 	ZS_Log_Count = ZS_Log_Count + 1
 	ZS_String = ZSRepeart( "#" , 10 )
 	ZS_String = ZS_String.." "..ZS_Log_Count.." ##\n"
 
-
 	local num_args = select("#", ...)
-	local classSelf,varName,value
+	local table,varName,value
 	if num_args == 1 then
 		local function getVarValue( name )
 			local value,found
@@ -96,63 +114,55 @@ function ZSLog(...)
 
 		varName = select(1, ...)
 		value = getVarValue(varName)
-		ZS_String = string.format("%s = %s",varName,tostring(value))
-		-- Log.Debug( string.format("%s = %s",varName,tostring(getVarValue(varName))) )
+		if type(value) == "table" then
+			ZSTable(value,varName)
+			return
+		else
+			ZS_String = ZS_String .. string.format("%s = %s",varName,tostring(value))			
+		end
 	elseif num_args == 2 then
-		classSelf = select(1, ...)
-		varName = select(2, ...)
-		value = classSelf[varName]
-		ZS_String = string.format("self.%s = %s",varName,tostring(value))
-		-- Log.Debug( string.format("self.%s = %s",varName ,classSelf[varName]) )
+		if type(select(1, ...)) == "table" then
+			table = select(1, ...)
+			varName = select(2, ...)
+			value = table[varName]
+			if type(value) == "table" then
+				ZSTable(value,string.format("Table.%s",varName))
+				return
+			elseif tonumber(varName) then
+				ZS_String = ZS_String .. string.format("Table[%s] = %s",tonumber(varName),tostring(value))
+			else
+				ZS_String = ZS_String .. string.format("Table.%s = %s",varName,tostring(value))
+			end
+		elseif type(select(1, ...)) == "string" then
+			varName = select(1, ...)
+			value = select(2, ...)
+			if type(value) == "table" then
+				ZSTable(value,varName)
+				return
+			else
+				ZS_String = ZS_String .. string.format("self.%s = %s",varName,tostring(value))
+			end
+		end
 	end
-
-	Log.Debug( string.format(ZSRepeart("<color=#%s>"),ZS_Log_Color)..ZS_String..ZSRepeart("</color>") )
+	local track_info = debug.getinfo(2, "Sln")
+	ZS_String = ZS_String..string.format("\nFrom %s:%d", track_info.short_src, track_info.currentline)
+	ZS_Debug_Log( ZS_String,ZS_Color_Log )
+	ZS_String = ""
 end
-
--- function ZSLog(string, color)
-	
--- function ZSLog(...)
-	-- Log.Debug(debug.getlocal (1, 1))
-	-- Log.Debug(debug.getlocal (1, 2))
-	-- Log.Debug(debug.getlocal (1, 3))
-	-- ZS_Log_Count = ZS_Log_Count + 1
-
-	-- -- printf("test")
-	-- ZS_String = ZSRepeart( ZS_Log_Count , 10 ).." "
-	-- local num_args = select("#", ...)
- --    for i = 1, num_args do
- --        local arg = select(i, ...)
- --        local track_info = debug.getlocal (1, arg)
- --        Log.Debug(track_info)
- --        -- for k,v in pairs(track_info) do
- --        -- 	Log.Debug(string.format("key:%s value:%s",k,v))
- --        -- end
- --        Log.Debug(ZS_String..string.format("%s:%s",i,arg))
- --    end
--- end
 
 function ZSProto(flag, cmd, data)
 	if ZSProtoFilter( cmd ) == false then return end
-	if flag == 1 then
-		ZS_String = ZSRepeart("<color=#88ffff>")..ZSRepeart(">>",10)
-	else
-		ZS_String = ZSRepeart("<color=#00ffff>")..ZSRepeart("<<<<",10)
-	end
-	ZS_String = ZS_String..ZSRepeart("proto ## ")..cmd..ZSRepeart(" ##")..ZSRepeart("</color>")
-	Log.Debug( ZS_String )
-
-	ZS_String = UtilsBase.serialize(data,nil,true)
-	-- ZS_String = ZS_String..ZSSerialize(data, nil, nil, 1, nil)
-
-	if flag == 1 then
-		Log.Debug( ZSRepeart("  ",22)..ZSRepeart("proto ## ")..cmd..ZSRepeart(" ##\n\n")..ZSRepeart("<color=#ffffff>")..ZS_String..ZSRepeart("</color>") )
-	else
-		Log.Debug( ZSRepeart("    ",22)..ZSRepeart("proto ## ")..cmd..ZSRepeart(" ##\n\n")..ZSRepeart("<color=#ffffff>")..ZS_String..ZSRepeart("</color>") )
-	end
+	ZS_String = ZSRepeart( flag==1 and ">>" or "<<<<",10)
+	ZS_String = ZS_String..ZSRepeart("proto ## ")..cmd..ZSRepeart(" ##\n")
+	ZS_String = ZS_String..ZSSerialize(data, nil, true)
+	ZS_Debug_Log( ZS_String,flag==1 and ZS_Color_Proto_Send or ZS_Color_Proto_Recv )
+	ZS_String = ""
 end
 
 function ZSTable(table,str)
-	-- UtilsBase.dump(table,str)
+	ZS_String = ZS_String..ZSSerialize(table,str,true)
+	ZS_Debug_Log( ZS_String,ZS_Color_Table )
+	ZS_String = ""
 end
 
 function ZSRepeart( repeartStr , n )
@@ -161,42 +171,6 @@ function ZSRepeart( repeartStr , n )
 		ZS_Repeart_String[ repeartStr.."_ZS_"..n ] = string.rep(repeartStr, n)
 	end
 	return ZS_Repeart_String[ repeartStr.."_ZS_"..n ]
-end
-
--- 序列化
--- 将传入的Obj 字符串化
-function ZSSerialize(obj, name, newline, depth, keyTab)
-	
-	local tempStr = ""
-	newline = newline == nil and true or newline
-	local space = newline and "		" or ""
-
-	if name then
-		tempStr = tempStr .. "name"
-		if type(name) == "number" then
-		end
-	end
-
-
-	if type(obj) == "number" then
-		tempStr = tempStr .. " = " .. tostring(obj)
-	elseif type(obj) == "string" then
-		tempStr = tempStr .. " = " .. obj
-	elseif type(obj) == "boolean" then
-		tempStr = tempStr .. " = " .. (obj and "true" or "false")
-	elseif type(obj) == "function" then
-		tempStr = tempStr .. "【function】"
-	elseif type(obj) == "userdata" then
-		tempStr = tempStr .. "【userdata】"
-	elseif type(obj) == "table" then
-		tempStr = tempStr .. " = {"
-		for k,v in pairs(obj) do
-			tempStr = tempStr .. ZSSerialize(v,k,newline,depth+1,keyTab)
-		end
-		tempStr = tempStr .. "}"
-	end
-
-	return tempStr
 end
 
 function ZSProtoFilter( cmd )
@@ -223,4 +197,101 @@ function ZSProtoFilter( cmd )
 	end
 
 	return not ZS_White_Flag
+end
+
+function ZS_Debug_Log( str,color )
+	local ind_s,ind_e = string.find(str, "\n")
+	if ind_s then
+		ind_s,ind_e = string.find(str, "\n",ind_e+1)
+	end
+	if ind_s then
+		Debug.Log( string.format(ZSRepeart("<color=%s>"),color)..string.sub(str,1,ind_s-1)..ZSRepeart("</color>").."\n"..string.format(ZSRepeart("<color=%s>"),color)..string.sub(str,ind_e+1)..ZSRepeart("</color>") )
+	else
+		Debug.Log( string.format(ZSRepeart("<color=%s>"),color)..str..ZSRepeart("</color>") )
+	end
+end
+
+-- 序列化
+-- 将传入的Obj 字符串化+
+function ZSSerialize(value, key, newline, depth, tableList)
+	local tempStr = ""
+	newline = newline == nil and true or newline
+	local space = "---| "
+	depth = depth == nil and 0 or depth
+	tableList = tableList == nil and {} or tableList
+
+	tempStr = tempStr .. ZSRepeart(space, depth)
+	if key then
+		if type(key) == "number" then
+			tempStr = tempStr .. "[" .. key .. "] = "
+		elseif type(key) == "string" then
+			tempStr = tempStr .. key .. " = "
+		else
+			tempStr = tempStr .. key
+		end
+	end
+
+	if type(value) == "number" or type(value) == "string" or type(value) == "boolean" then
+		tempStr = tempStr..tostring(value)
+	elseif type(value) == "function" then
+		tempStr = tempStr.."【function】"
+	elseif type(value) == "userdata" then
+		tempStr = tempStr.."【userdata】"
+	elseif type(value) == "table" and tableList[value] == nil then
+		if next(value) then
+			tempStr = tempStr .."<"..tostring(value).."> {"..( newline and "\n" or " ")
+			tableList[value] = key
+			-- local tempKey,tempValue = next(value)
+			-- while( tempKey ) do
+			-- 	tempStr = tempStr..ZSSerialize(tempValue,tempKey,newline,depth+1,tableList)
+			-- 	tempKey,tempValue = next(value,tempKey)
+			-- end
+			for tempKey,tempValue in pairs(value) do
+				tempStr = tempStr..ZSSerialize(tempValue,tempKey,newline,depth+1,tableList)..","..( newline and "\n" or " ")
+			end
+			tempStr = tempStr..ZSRepeart(space, depth).."}"
+		else
+			tempStr = tempStr .."<"..tostring(value).."> {}"
+		end
+	else--重复table(所指地址相同)
+		tempStr = tempStr.."【"..tableList[value].."】<"..tostring(value)..">{...}"
+	end
+	return tempStr
+end
+
+-- 反序列化
+-- function ZSUnserialize(str)
+--     return assert(loadstring("local tmp = " .. str .. " return tmp"))()
+-- end
+
+function ZSColorShow()
+	local r = 0
+	local g = 0
+	local b = 0xf
+	local color
+	for i = 0, 0x3f do
+		if i < 0x10 then
+			g = i
+		elseif i < 0x20 then
+			b = 0x1f - i
+		elseif i < 0x30 then
+			r = i - 0x20
+		elseif i <= 0x3f then
+			g = 0x3f- i
+		end
+		color = r*0x110000+g*0x1100+b*0x11
+		ZS_String = ZS_String..string.format("\n<color=#%.6x>#%.6x</color>",color,color)
+		-- ZS_String = ZS_String..string.format("<color=#%.6x>#</color>",color)
+	end
+	ZS(ZS_String)
+
+	--打印显示
+	local table = { [1] = 1, a = "a" }
+	ZS()
+	ZS("ZS(...)")
+    ZSLog("table")
+    ZSLog(table,"a")
+    ZSTable(table,"table")
+    ZSProto(1,100,{})
+    ZSProto(2,111,{})
 end
